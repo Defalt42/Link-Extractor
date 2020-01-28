1d8ab4d843129ff0c560fbf85ea4c9097d7df121
defalt42 <gilbertb4375@gmail.com>
2020-01-28 13:09:11 -0600

    Add error handling

--- scrape.py
+++ scrape.py
@@ -2,3 +2,25 @@ from bs4 import BeautifulSoup
 import requests
 
 url = input("Enter the URL you would like to scrape: ")
+
+page = requests.get(url)
+
+data = page.text
+
+soup = BeautifulSoup(data, "html.parser")
+
+links_with_index = []
+
+for link in soup.find_all("a"):
+    if link.get("href") is not None:
+        if "index.html" in link.get("href"):
+            links_with_index.append(link.get("href"))
+try:
+    links_file = open("links.txt", "w")
+
+    for link in links_with_index:
+        links_file.write(link + "\n")
+
+    links_file.close()
+except IOError:
+    print("Error opening or writing to file")
